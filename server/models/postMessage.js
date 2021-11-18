2e22247a9ca26be3d74716a74fcf478101bdd54d
akash5100 <shivamvermashs@gmail.com>
2021-11-18 10:35:15 +0530

    added server and client side

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -1,7 +1,19 @@
-import { Mongoose } from "mongoose";
+import mongoose from "mongoose";
 
-const postSchema = Mongoose.Schema({
+const postSchema = mongoose.Schema({
   title: String,
   message: String,
   creator: String,
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
 });
+
+const PostMessage = mongoose.model("PostMessage", postSchema);
+
+export default PostMessage;
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getPosts } from "../controller/posts.js";
+import { getPosts, createPost } from "../controller/posts.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
+router.get("/", createPost);
 
 export default router;
