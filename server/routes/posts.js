752e677dfb079642ca51091b9cc53def9630271f
akash5100 <shivamvermashs@gmail.com>
2021-11-22 16:01:53 +0530

    post request successfull

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -4,6 +4,6 @@ import { getPosts, createPost } from "../controller/posts.js";
 const router = express.Router();
 
 router.get("/", getPosts);
-router.get("/", createPost);
+router.post("/", createPost);
 
 export default router;
