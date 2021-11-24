5243c08565c80ed5aec2b48bb06274848eb4afc3
akash5100 <shivamvermashs@gmail.com>
2021-11-24 17:29:57 +0530

    improvement

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
 import postRouter from "./routes/posts.js";
 
 const app = express();
-
+dotenv.config();
 app.use(bodyParser.json({ extended: true }));
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -14,15 +14,11 @@ app.use(cors({ origin: "*" }));
 
 app.use("/posts", postRouter);
 
-//temp gonna store this in virtual env later
-const CONNECTION_URL =
-  "mongodb+srv://tweesy101:emmawatson101@cluster0.94m6o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
 const port = process.env.PORT || 5000;
 
 // these are not required but rather have it else it will show error in console
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
